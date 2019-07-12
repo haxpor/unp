cccd6476a5052d87182bed6d9f3f10642b80d2fb
Wasin Thonkaew <haxpor@gmail.com>
2019-07-13 05:34:54 +0800

    complete daytimeclient in chapter 1

--- src/chapter_1/DaytimeClient.cpp
+++ src/chapter_1/DaytimeClient.cpp
@@ -13,5 +13,34 @@ int main( int argc, char **argv ) {
     if ( argc != 2 ) {
         unp::ErrorQuit( "Usage: ch1_daytimeclient <IPAddress>" ); 
     }
+
+    if ( (sockfd = socket( AF_INET, SOCK_STREAM, 0 )) < 0 ) {
+        unp::ErrorSys( "Socket error" );
+    }
+
+#ifdef HAVE_STRINGS_H
+    bzero( &servaddr, sizeof(servaddr) );
+#else
+    memset( &servaddr, 0, sizeof(servaddr) );
+#endif
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13);      // need to convert to network byte order
+                                        // network byte order is always in big-endian
+    if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0 ) {
+        unp::ErrorQuit( "Connect error" );
+    }
+
+    while ( (n = read( sockfd, recvline, MAXLINE )) > 0 ) {
+        recvline[n] = 0;                // set null-terminated to read line
+        if ( std::fputs( recvline, stdout ) == EOF ) {
+            unp::ErrorSys( "fputs error" );
+        }
+    }
+
+    if ( n < 0 ) {
+        unp::ErrorSys( "read error" );
+    }
+
     return 0;
 }
