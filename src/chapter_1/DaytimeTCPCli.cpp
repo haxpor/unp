f57bb8503bee339fd78bc65a75a513105f7d7365
Wasin Thonkaew <haxpor@gmail.com>
2019-07-13 06:12:14 +0800

    fix connection and add better handling of response from daytime server

--- src/chapter_1/DaytimeTCPCli.cpp
+++ src/chapter_1/DaytimeTCPCli.cpp
@@ -6,7 +6,7 @@ int main( int argc, char **argv ) {
     unp::daemonProc = 0;
 
     int sockfd, n;
-    char recvline[MAXLINE + 1];
+    char recvline[MAXLINE + 1] = {0};
 
     struct sockaddr_in servaddr;
 
@@ -19,21 +19,35 @@ int main( int argc, char **argv ) {
     }
 
 #ifdef HAVE_STRINGS_H
-    bzero( &servaddr, sizeof(servaddr) );
+    bzero( &servaddr, sizeof( servaddr ) );
 #else
-    memset( &servaddr, 0, sizeof(servaddr) );
+    memset( &servaddr, 0, sizeof( servaddr ) );
 #endif
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(13);      // need to convert to network byte order
                                         // network byte order is always in big-endian
     if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0 ) {
-        unp::ErrorQuit( "Connect error" );
+        unp::ErrorQuit( "inet_pton error for %s", argv[1]);
+    }
+
+    if ( connect( sockfd, (const SA *)&servaddr, sizeof( servaddr ) ) == -1 ) {
+        unp::ErrorSys( "Connect error" );
     }
 
     while ( (n = read( sockfd, recvline, MAXLINE )) > 0 ) {
         recvline[n] = 0;                // set null-terminated to read line
-        if ( std::fputs( recvline, stdout ) == EOF ) {
+
+        /* trim newline at the beginning */
+        int offset = 0;
+        if ( recvline[0] == '\n' ) {
+            offset = 1;
+        } else if ( strlen( recvline ) > 2 &&
+                    recvline[0] == '\r' && recvline[1] == '\n' ) {
+            offset = 2;
+        }
+
+        if ( std::fputs( recvline + offset, stdout ) == EOF ) {
             unp::ErrorSys( "fputs error" );
         }
     }
